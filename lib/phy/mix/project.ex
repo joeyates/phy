76ba488d94c5baddec1b41806aa8d40d59d0290d
Joe Yates <joe.g.yates@gmail.com>
2024-01-28 11:11:09 +0100

    Fill out tests

--- lib/phy/mix/project.ex
+++ lib/phy/mix/project.ex
@@ -1,25 +1,34 @@
 defmodule Phy.Mix.Project do
+  @moduledoc """
+  This module wraps access to Mix.Project,
+  allowing for easier mocking elsewhere in the project.
+  While the rest of the project uses Mox, this wrapper uses
+  dependency injection as its testing/mocking approach.
+  """
   @callback config() :: map()
-  @callback config(any()) :: map()
+  @callback config(mix_project :: Mix.Project) :: map()
   def config(mix_project \\ Mix.Project) do
     mix_project.config()
   end
 
   @callback app() :: atom()
-  def app do
-    config()[:app]
+  @callback app(mix_project :: Mix.Project) :: atom()
+  def app(mix_project \\ Mix.Project) do
+    Keyword.fetch!(config(mix_project), :app)
   end
 
-  @callback module() :: atom()
-  def module do
-    app()
+  @callback module() :: String.t()
+  @callback module(mix_project :: Mix.Project) :: String.t()
+  def module(mix_project \\ Mix.Project) do
+    app(mix_project)
     |> Atom.to_string()
     |> Macro.camelize()
   end
 
-  @callback web_module() :: atom()
-  def web_module do
-    app()
+  @callback web_module() :: String.t()
+  @callback web_module(mix_project :: Mix.Project) :: String.t()
+  def web_module(mix_project \\ Mix.Project) do
+    app(mix_project)
     |> then(&"#{&1}_web")
     |> Macro.camelize()
   end
--- test/phy/mix/project_test.exs
+++ test/phy/mix/project_test.exs
@@ -1,6 +1,6 @@
 defmodule MixProjectMock do
   def config do
-    %{the: :config}
+    [app: :my_app]
   end
 end
 
@@ -9,7 +9,25 @@ defmodule Phy.Mix.ProjectTest do
 
   describe "config/0" do
     test "it reads the config" do
-      assert Phy.Mix.Project.config(MixProjectMock) == %{the: :config}
+      assert Phy.Mix.Project.config(MixProjectMock) == [app: :my_app]
+    end
+  end
+
+  describe "app/0" do
+    test "it returns the app name" do
+      assert Phy.Mix.Project.app(MixProjectMock) == :my_app
+    end
+  end
+
+  describe "module/0" do
+    test "it camelizes the app name" do
+      assert Phy.Mix.Project.module(MixProjectMock) == "MyApp"
+    end
+  end
+
+  describe "web_module/0" do
+    test "it adds 'Web' to the module" do
+      assert Phy.Mix.Project.web_module(MixProjectMock) == "MyAppWeb"
     end
   end
 end
