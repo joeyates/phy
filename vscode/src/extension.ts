d18a8cc133a87744510a6a082c6591e94f009ef0
Joe Yates <joe.g.yates@gmail.com>
2024-03-15 09:43:11 +0100

    Extract switcher to another extension

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,13 +1,6 @@
 import * as vscode from 'vscode';
-import * as switcher from './switcher';
 
 export function activate(context: vscode.ExtensionContext) {
-  const toggleCommand = vscode.commands.registerCommand(
-    'extension.phy.switch',
-    () => switcher.switchBetweenImplementationAndTest()
-  );
-
-  context.subscriptions.push(toggleCommand);
 }
 
 export function deactivate() {}
